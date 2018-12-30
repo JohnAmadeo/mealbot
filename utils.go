01b008d347562b229052b66bf41e34fe0b6af302
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-12-30 08:49:51 +0700

    Util for extracting query params (needed by handlers)

---
+++ utils.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+func getQueryParam(r *http.Request, key string) (string, error) {
+	queries, ok := r.URL.Query()[key]
+	if !ok || len(queries) > 1 {
+		return "", errors.New("Request query parameters must contain admin")
+	}
+	return queries[0], nil
+}

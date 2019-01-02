41bae31245994a1b8192ea0f5841ef40e426bbe8
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2019-01-02 21:48:17 +0700

    Comment out server for Heroku test

--- server.go
+++ server.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"os"
 )
 
 type Middleware struct {
@@ -28,26 +27,36 @@ func (mw Middleware) ApplyFake(
 }
 
 func main() {
-	mw := Middleware{
-		MiddlewareHandlers: [](func(handler http.Handler) http.Handler){
-			GetAuthHandler,
-			GetCorsHandler,
-		},
-	}
+	fmt.Println("Hello!")
+	// createOrganization("ysc", "johnamadeo.daniswara@yale.edu")
+	// createMembersFromCSV("ysc", filepath.Join(build.Default.GOPATH, CSVPath, "ysc.csv"))
+	// addRound("ysc", "1999-01-08 04:05:06")
+	// rescheduleRound("ysc", "1999-10-08 04:05:06", 0)
+	// addRound("ysc", "2019-01-08 04:05:06")
+	// runPairingRound("ysc", 0, true)
+	// runPairingRound("ysc", 1, true)
 
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/members", mw.Apply(MembersHandler))
-	serveMux.Handle("/orgs", mw.Apply(GetOrganizationsHandler))
-	serveMux.Handle("/org", mw.Apply(CreateOrganizationHandler))
-	serveMux.Handle("/crossmatchtrait", mw.Apply(CrossMatchTraitHandler))
-	serveMux.Handle("/rounds", mw.Apply(GetRoundsHandler))
-	serveMux.Handle("/round", mw.Apply(RoundHandler))
-	serveMux.Handle("/", http.FileServer(http.Dir("./static")))
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Fatal(http.ListenAndServe(":"+port, serveMux))
+	// mw := Middleware{
+	// 	MiddlewareHandlers: [](func(handler http.Handler) http.Handler){
+	// 		GetAuthHandler,
+	// 		GetCorsHandler,
+	// 	},
+	// }
+	//
+	// serveMux := http.NewServeMux()
+	// serveMux.Handle("/members", mw.Apply(MembersHandler))
+	// serveMux.Handle("/orgs", mw.Apply(GetOrganizationsHandler))
+	// serveMux.Handle("/org", mw.Apply(CreateOrganizationHandler))
+	// serveMux.Handle("/crossmatchtrait", mw.Apply(CrossMatchTraitHandler))
+	// serveMux.Handle("/rounds", mw.Apply(GetRoundsHandler))
+	// serveMux.Handle("/round", mw.Apply(RoundHandler))
+	// serveMux.Handle("/pairs", mw.Apply(GetPairsHandler))
+	// serveMux.Handle("/", http.FileServer(http.Dir("./static")))
+	//
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
+	//
+	// log.Fatal(http.ListenAndServe(":"+port, serveMux))
 }

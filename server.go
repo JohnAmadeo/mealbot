ca5edc9adfd155adf4a222d5311f91144d2d9bcb
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2019-01-02 21:58:56 +0700

    Test scheduler entry point

--- server.go
+++ server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"go/build"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 type Middleware struct {
@@ -27,36 +30,39 @@ func (mw Middleware) ApplyFake(
 }
 
 func main() {
-	fmt.Println("Hello!")
-	// createOrganization("ysc", "johnamadeo.daniswara@yale.edu")
-	// createMembersFromCSV("ysc", filepath.Join(build.Default.GOPATH, CSVPath, "ysc.csv"))
-	// addRound("ysc", "1999-01-08 04:05:06")
-	// rescheduleRound("ysc", "1999-10-08 04:05:06", 0)
-	// addRound("ysc", "2019-01-08 04:05:06")
-	// runPairingRound("ysc", 0, true)
-	// runPairingRound("ysc", 1, true)
+	args := os.Args
+	if len(args) == 2 && args[1] == "" {
+		createOrganization("ysc", "johnamadeo.daniswara@yale.edu")
+		createMembersFromCSV("ysc", filepath.Join(build.Default.GOPATH, CSVPath, "ysc.csv"))
+		addRound("ysc", "1999-01-08 04:05:06")
+		rescheduleRound("ysc", "1999-10-08 04:05:06", 0)
+		addRound("ysc", "2019-01-08 04:05:06")
+		runPairingRound("ysc", 0, true)
+		runPairingRound("ysc", 1, true)
+		return
+	}
 
-	// mw := Middleware{
-	// 	MiddlewareHandlers: [](func(handler http.Handler) http.Handler){
-	// 		GetAuthHandler,
-	// 		GetCorsHandler,
-	// 	},
-	// }
-	//
-	// serveMux := http.NewServeMux()
-	// serveMux.Handle("/members", mw.Apply(MembersHandler))
-	// serveMux.Handle("/orgs", mw.Apply(GetOrganizationsHandler))
-	// serveMux.Handle("/org", mw.Apply(CreateOrganizationHandler))
-	// serveMux.Handle("/crossmatchtrait", mw.Apply(CrossMatchTraitHandler))
-	// serveMux.Handle("/rounds", mw.Apply(GetRoundsHandler))
-	// serveMux.Handle("/round", mw.Apply(RoundHandler))
-	// serveMux.Handle("/pairs", mw.Apply(GetPairsHandler))
-	// serveMux.Handle("/", http.FileServer(http.Dir("./static")))
-	//
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// }
-	//
-	// log.Fatal(http.ListenAndServe(":"+port, serveMux))
+	mw := Middleware{
+		MiddlewareHandlers: [](func(handler http.Handler) http.Handler){
+			GetAuthHandler,
+			GetCorsHandler,
+		},
+	}
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/members", mw.Apply(MembersHandler))
+	serveMux.Handle("/orgs", mw.Apply(GetOrganizationsHandler))
+	serveMux.Handle("/org", mw.Apply(CreateOrganizationHandler))
+	serveMux.Handle("/crossmatchtrait", mw.Apply(CrossMatchTraitHandler))
+	serveMux.Handle("/rounds", mw.Apply(GetRoundsHandler))
+	serveMux.Handle("/round", mw.Apply(RoundHandler))
+	serveMux.Handle("/pairs", mw.Apply(GetPairsHandler))
+	serveMux.Handle("/", http.FileServer(http.Dir("./static")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, serveMux))
 }

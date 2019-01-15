7697f0e444dead687a744bb6b9c1bd5aceb84d49
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2019-01-15 10:16:52 -0500

    TEMPORARY CORS debugging setup; remove before launch

--- cors.go
+++ cors.go
@@ -5,14 +5,17 @@ import (
 )
 
 const (
-	// AcessControlAllowOrigin = "http://localhost:3000"
-	AccessControlAllowOrigin  = "https://mealbot-web.herokuapp.com"
+	// TODO: Get rid of temporary AccessControlAllowOrigin debugging setup
+	// AccessControlAllowOrigin = "http://localhost:3000"
+	// AccessControlAllowOrigin  = "https://mealbot-web.herokuapp.com"
 	AccessControlAllowHeaders = "Authorization, Content-Type, Origin, Accept, token"
 )
 
 func GetCorsHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", AccessControlAllowOrigin)
+
+		// TODO: Get rid of temporary AccessControlAllowOrigin debugging setup
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Add("Access-Control-Allow-Headers", AccessControlAllowHeaders)
 		w.Header().Add("Access-Control-Allow-Methods", "GET, POST, DELETE")
 
